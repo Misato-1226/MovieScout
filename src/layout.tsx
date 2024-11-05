3618d87090b31ce8df8084326053594c045f5c3c
Misato <rexlove1226@gmail.com>
2024-11-05 15:37:31 +0900

    fix props type in layout.tsx

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,10 @@
+import { ReactNode } from "react";
+
+type LayoutProps = {
+  children: ReactNode;
+};
 // src/components/Layout.jsx
-const Layout = ({ children }) => {
+const Layout = ({ children }: LayoutProps) => {
   return <div className="bg-blue-950 text-white min-h-screen">{children}</div>;
 };
 
